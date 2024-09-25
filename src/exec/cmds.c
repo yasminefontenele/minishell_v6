5f81ab38a70f7b3ad5c44449e86f36596dd5f2e0
EliskaM <eliskam@eliska.eliska>
2024-09-25 09:20:12 +0100

    command not found fixed, forking checked, still issue with CD ;/

--- src/exec/cmds.c
+++ src/exec/cmds.c
@@ -6,7 +6,7 @@
 /*   By: eliskam <eliskam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 16:34:30 by emencova          #+#    #+#             */
-/*   Updated: 2024/09/25 08:40:06 by eliskam          ###   ########.fr       */
+/*   Updated: 2024/09/25 09:18:31 by eliskam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,149 @@
 #include "../../minishell.h"
 #include <dirent.h>
 
+static char *cmd_find(char **path_env, char *comnd)
+{
+    char *tmp;
+    char *path_full;
+    int i;
+    
+    i = 0;
+    path_full = NULL;
+    if (!path_env || !comnd)
+        return (NULL);
+    while (path_env[i])
+    {
+        tmp = ft_strjoin(path_env[i], "/");
+        if (!tmp)
+            return (NULL);
+        path_full = ft_strjoin(tmp, comnd);
+        free(tmp);
+        if (!path_full)
+            return (NULL);
+        if (access(path_full, F_OK) == 0)
+            return (path_full);
+        free(path_full);
+        path_full = NULL;
+        i++;
+    }
+    return (NULL);
+}
 
+static DIR *check_cmd(t_shell *shell, t_list *comnd, char ***str)
+{
+    t_exec *node;
+    DIR *directory;
+    char *path_env;
+
+    node = comnd->content;
+    directory = NULL;
+    if (!node || !node->args)
+        return (NULL);
+    directory = opendir(*node->args);
+    if (directory)
+        return (directory);
+    if (ft_strchr(*node->args, '/') && !directory)
+    {
+        *str = ft_split(*node->args, '/');
+        if (!*str)
+            m_error(ERR_NEWCMD, "Failed to split command", 127);
+        node->path = ft_strdup(*node->args);
+        free(node->args[0]);
+        node->args[0] = ft_strdup((*str)[form_len(*str) - 1]);
+        return (NULL);
+    }
+    path_env = get_env("PATH", shell->keys, 4);
+    *str = ft_split(path_env, ':');
+    free(path_env);
+    if (!*str)
+        m_error(ERR_NEWCMD, "Failed to split PATH", 127);
+    node->path = cmd_find(*str, *node->args);
+    if (!node->path)
+        return (NULL);
+
+    return (NULL);
+}
+
+
+void command_get(t_shell *shell, t_list *comnd)
+{
+    t_exec *node;
+    DIR *directory;
+    char **str;
+    
+    str = NULL;
+    node = comnd->content; 
+    if (built_check(node))
+    {
+        builtin(shell, comnd, &g_env.exit_status, ft_strlen(node->args[0]));
+        return;
+    }
+    directory = check_cmd(shell, comnd, &str);
+    if (directory)
+    {
+        closedir(directory);
+        return;
+    }
+    if (!node->path || access(node->path, X_OK) != 0)
+    {
+        fprintf(stderr, "command not found: %s\n", node->args[0]);
+        free_form(&str);
+        return;
+    }
+    if (!check_fork(shell, comnd, NULL))
+        return;
+    free_form(&str);
+}
+
+void cmd_execute(t_shell *shell, t_list *commands_list)
+{
+    int fd[2];
+    t_list *cmd_node = commands_list;
+    t_exec *current_cmd;
+    t_exec *next_cmd;
+
+    while (cmd_node)
+    {
+        current_cmd = (t_exec *)cmd_node->content;
+        if (!current_cmd)
+        {
+            fprintf(stderr, "Error: current_cmd is null in cmd_execute\n");
+            return;
+        }
+        if (cmd_node->next)
+            next_cmd = (t_exec *)cmd_node->next->content;
+        else
+            next_cmd = NULL;
+        command_get(shell, cmd_node);
+        if (next_cmd)
+        {
+            if (pipe(fd) == -1)
+            {
+                m_error(ERR_PIPE, NULL, 1);
+                return;
+            }
+            if (current_cmd->out > 2)
+                close(current_cmd->out);
+            if (next_cmd->in == -1) // Assuming -1 means it hasn't been set
+                next_cmd->in = fd[PIPE_READ];
+            close(fd[PIPE_WRITE]);
+        }
+        else
+        {
+            if (current_cmd->out > 2)
+                close(current_cmd->out);
+        }
+        cmd_node = cmd_node->next;
+    }
+}
+
+
+
+
+
+
+
+/*
 static char	*cmd_find(char **path_env, char *comnd, char *path_full)
 {
 	char	*tmp;
@@ -73,7 +215,7 @@ static DIR	*check_cmd(t_shell *shell, t_list *comnd, char ***str)
 	}
 	return (directory);
 }
-/*
+
 void command_get(t_shell *shell, t_list *comnd)
 {
     t_exec *node;
@@ -110,54 +252,6 @@ void command_get(t_shell *shell, t_list *comnd)
         m_error(ERR_NEWDIR, node->args[0], 127);
     free_form(&str);
 }
-*/
-void command_get(t_shell *shell, t_list *comnd)
-{
-    t_exec *node;
-    DIR *directory;
-    char **str;
-
-    str = NULL;
-    node = comnd->content;
-
-    // Check for built-in commands
-    if (built_check(node))
-    {
-        builtin(shell, comnd, &g_env.exit_status, ft_strlen(node->args[0]));
-        return;
-    }
-
-    // Check if it's a directory or command
-    directory = check_cmd(shell, comnd, &str);
-    if (directory) {
-        closedir(directory);
-        return;
-    }
-
-    // If the command path is not found, print the error once
-    if (!node->path || access(node->path, X_OK) != 0) {
-        // Print the error once
-        fprintf(stderr, "command not found: %s\n", node->args[0]);
-        free_form(&str);
-        return;
-    }
-
-    // Fork and execute the valid command
-    pid_t pid = fork();
-    if (pid < 0) {
-        m_error(ERR_FORK, NULL, 1);
-    }
-    else if (pid == 0) {
-        execve(node->path, node->args, shell->keys);
-        m_error(ERR_ISDIR, node->args[0], 126);  // If execve fails, exit with an error
-    }
-    else {
-        waitpid(pid, &g_env.exit_status, 0);  // Parent process waits for the child
-    }
-
-    free_form(&str);
-}
-
 
 void cmd_execute(t_shell *shell, t_list *commands_list)
 {
@@ -180,6 +274,7 @@ void cmd_execute(t_shell *shell, t_list *commands_list)
             next_cmd = (t_exec *)cmd_node->next->content;
         else
             next_cmd = NULL;
+            
         command_get(shell, cmd_node);
 
         if (next_cmd)
@@ -202,3 +297,4 @@ void cmd_execute(t_shell *shell, t_list *commands_list)
         cmd_node = cmd_node->next;
     }
 }
+*/
--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,14 +6,14 @@
 /*   By: eliskam <eliskam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 22:31:57 by emencova          #+#    #+#             */
-/*   Updated: 2024/09/25 08:44:11 by eliskam          ###   ########.fr       */
+/*   Updated: 2024/09/25 09:01:36 by eliskam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 #include <dirent.h>
 
-
+/*
 static void	handle_redirect(t_list *cmd_node, int pipes[2])
 {
     t_exec *node;
@@ -83,7 +83,7 @@ int check_fork(t_shell *shell, t_list *cmd, int fd[2])
     else
     return (1);
 }
-/*
+
 void *prepare_exec(t_shell *shell, t_list *cmd, int fd[2])
 {
 	t_exec *cmd_info;
@@ -114,3 +114,70 @@ void *prepare_exec(t_shell *shell, t_list *cmd, int fd[2])
 */
 
 
+static void handle_redirect(t_list *cmd_node, int pipes[2])
+{
+    t_exec *node = cmd_node->content;
+
+    // Redirect input
+    if (node->in != STDIN_FILENO)
+    {
+        if (dup2(node->in, STDIN_FILENO) == -1)
+        {
+            m_error(ERR_DUP, NULL, 1);
+            return;
+        }
+        close(node->in);
+    }
+
+    // Redirect output
+    if (node->out != STDOUT_FILENO)
+    {
+        if (dup2(node->out, STDOUT_FILENO) == -1)
+        {
+            m_error(ERR_DUP, NULL, 1);
+            return;
+        }
+        close(node->out);
+    }
+    else if (cmd_node->next) // If there is a next command, redirect to the pipe
+    {
+        if (dup2(pipes[PIPE_WRITE], STDOUT_FILENO) == -1)
+        {
+            m_error(ERR_DUP, NULL, 1);
+            return;
+        }
+    }
+}
+
+void handle_process(t_shell *shell, t_list *cmd, int fd[2])
+{
+    handle_redirect(cmd, fd);
+    close(fd[PIPE_READ]); // Close read end in the child process
+    // Execute built-in commands directly if needed, for example:
+    // if (is_builtin(cmd))
+    //     execute_builtin(shell, cmd);
+    // else
+    //     execve(node->path, node->args, shell->keys);
+    
+    ft_lstclear(&shell->cmds, ft_free); // Clear command list
+    exit(g_env.exit_status); // Exit with the exit status
+}
+
+int check_fork(t_shell *shell, t_list *cmd, int fd[2])
+{
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        close(fd[0]);
+        close(fd[1]);
+        m_error(ERR_FORK, NULL, 1);
+        return 0; // Indicate an error
+    }
+    else if (pid == 0) // Child process
+    {
+        handle_process(shell, cmd, fd); // Redirect and handle the command
+    }
+    // Parent process continues
+    return 1; // Indicate success
+}
